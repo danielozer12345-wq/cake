0417f6e30395f7ad1f4d4180a445a05f6a107ee5
danielozer12345-wq <danielozer12345@gmail.com>
2026-02-01 12:57:34 +0200

    Updated favicon and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Gallery from './pages/Gallery'
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <myContext.Provider value={{ addProducts, deleteProduct, updateQuantity, products, totalAmount }}>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -54,7 +54,7 @@ function App() {
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
         </a>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
